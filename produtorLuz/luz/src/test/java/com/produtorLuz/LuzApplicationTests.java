caff8f92a8f6ea0b14d0431fe49dd1322ccaa505
ana010109 <achausmann@furb.br>
2025-11-09 15:07:37 -0300

    producer da luz base

---
+++ produtorLuz/luz/src/test/java/com/produtorLuz/LuzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.produtorLuz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
